c3cff66c2a285da9ed923d25e128203956affde2
Archana_06 <238r1a05v6@cmrec.ac.in>
2024-11-12 10:26:44 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.scanner
+  class java {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter your rollno");
+      int rollno=sc.nextInt();
+    }
+  }
